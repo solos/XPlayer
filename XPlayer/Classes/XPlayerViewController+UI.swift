bb12718009f6e6c4699a35dad834256cf312d5e2
solos <lxl1217@gmail.com>
2019-12-10 16:01:20 +0800

    update
    update

--- XPlayer/Classes/XPlayerViewController+UI.swift
+++ XPlayer/Classes/XPlayerViewController+UI.swift
@@ -45,9 +45,9 @@ extension XPlayerViewController {
         fullScreenButton.trailingToSuperview(offset: leftMargin)
         fullScreenButton.bottomToSuperview(offset: -bottomMargin)
 
-        closeButton.size(CGSize.square(24))
+        closeButton.size(CGSize.square(32))
         closeButton.trailingToSuperview(offset: leftMargin)
-        closeButton.topToSuperview(offset: 50)
+        closeButton.topToSuperview(offset: 35)
 
         timelineLabel.centerY(to: fullScreenButton)
         timelineLabel.trailingToLeading(of: fullScreenButton, offset: -leftMargin)
